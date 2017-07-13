28ce473aa64b6a5d53929afe0a25d05e6f253840
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2017-07-12 23:40:07 -0300

    deu certo

--- src/main/java/organizador/si/Usuario.java
+++ src/main/java/organizador/si/Usuario.java
@@ -1,18 +1,14 @@
-package organizador.si;/**
- * Created by matheus on 11/07/17.
- */
+package organizador.si;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class UsuarioRest {
-    @RequestMapping(value="/teste", method=RequestMethod.GET)
-    public String helloWord() {
-        return "Oi, eu estou funcionando!";
-    }
+public class Usuario {
 
 
 
 }
+
